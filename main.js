6a7d93b0bccee9ac734f884de93075610bc579e9
Riccardo Belli <riccardo_belli@icloud.com>
2020-03-20 10:46:57 +0100

    fix

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var euroPerKM = 0.21;
 
 var scontoUnder18 = 0.2;
 var scontoOver65 = 0.4;
+
 var scontoBiglietto;
 
 // Chiedere all'utente quanti KM deve percorrere
@@ -18,9 +19,9 @@ var costoBiglietto = kmDaPercorrere * euroPerKM;
 // Calcolo biglietti con eventuali sconti
 
 if (etaUtente < 18) {
-  scontoBiglietto = costoBiglietto * scontoUnder18;
+  scontoBiglietto = costoBiglietto * 0.8;
 } else if (etaUtente > 65) {
-  scontoBiglietto = costoBiglietto * scontoOver65;
+  scontoBiglietto = costoBiglietto * 0.6;
 } else {
   scontoBiglietto = costoBiglietto;
 }
